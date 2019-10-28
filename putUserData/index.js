a6a75340bde8d98a3f29e2a330eb8e490959a670
jawad727 <jawaddhussein@gmail.com>
2019-10-28 01:22:42 -0700

    putUserData function updated to return response body and status code

--- putUserData/index.js
+++ putUserData/index.js
@@ -4,23 +4,39 @@ const AWS = require('aws-sdk');
 AWS.config.update({region: "us-west-2"});
 
 exports.handler = async (event, context) => {
-    const ddb = new AWS.DynamoDB();
     const documentClient = new AWS.DynamoDB.DocumentClient({ region: "us-west-2" })
+
+    let responseBody = "";
+    let statusCode = 0
+
+    const { user_id, first_name, age, last_name } = JSON.parse(event.body)
 
     const params = {
         TableName: "Users",
         Item: {
-            user_id: "d16fg6",
-            first_name: "Tester",
-            age: 21,
-            last_name: "Testing"
+            user_id: user_id,
+            first_name: first_name,
+            age: age,
+            last_name: last_name
         }
     };
 
     try {
          const data = await documentClient.put(params).promise()
-         console.log(data)
+         responseBody = JSON.stringify(data);
+         statusCode = 201;
     } catch(err) {
-        console.log(err)
+        responseBody = `Unable to put product: ${err}`
+        statusCode = 403
     }
+
+    const response = {
+        statusCode: statusCode,
+        headers: {
+            "Content-Type": "application/json"
+          },
+        body: responseBody
+    }
+
+    return response
 };
