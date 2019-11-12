55120c2fcc08100b24d4590df68c194723d12e28
jawad727 <jawaddhussein@gmail.com>
2019-11-11 23:44:41 -0800

    base64 encode s3 lambda

--- s3lambda/index.js
+++ s3lambda/index.js
@@ -1,14 +1,22 @@
-
-'use strict';
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3()
-const documentClient = new AWS.DynamoDB.DocumentClient();
+const moment = require("moment")
+const fileType = require("file-type")
 
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+exports.handler = async (event, context) => {
+    let request = event.body;
+    let base64String = request.base64String
+    let buffer = new Buffer(base64String, "base64")
+    let fileMime = fileType(buffer)
+    let file = getFile(fileMime, buffer)
+    let params = file.params
+
+    s3.putObject(params, function(err, data) {
+        if (err) {
+            return console.log(err)
+        }
+
+        return console.log("File URL", file.full_path)
+    })
+
 };
